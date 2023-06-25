fec58b09ad087ce1b76f1a0dd9d8f86f5b294cf4
Paul KH KIM <babo_81@hotmail.com>
2023-06-26 09:27:01 +1200

    Fixed indentation

--- TNZAPI.NET/Core/Interfaces/Addressbook/IGroupApi.cs
+++ TNZAPI.NET/Core/Interfaces/Addressbook/IGroupApi.cs
@@ -7,25 +7,54 @@ namespace TNZAPI.NET.Core.Interfaces.Addressbook
         GroupModel Entity { get; set; }
 
         GroupApiResult Create(GroupModel entity);
-        GroupApiResult Create(string groupCode = null, string groupName = null, string subaccount = null, string department = null);
+        GroupApiResult Create(
+            string groupCode = null, 
+            string groupName = null, 
+            string subaccount = null, 
+            string department = null
+        );
+
         Task<GroupApiResult> CreateAsync(GroupModel entity);
-        Task<GroupApiResult> CreateAsync(string groupCode = null, string groupName = null, string subaccount = null, string department = null);
+        Task<GroupApiResult> CreateAsync(
+            string groupCode = null, 
+            string groupName = null, 
+            string subaccount = null, 
+            string department = null
+        );
+
         GroupApiResult Delete(GroupModel entity);
         GroupApiResult DeleteByGroupCode(string groupCode);
+
         Task<GroupApiResult> DeleteAsync(GroupModel entity);
         Task<GroupApiResult> DeleteByGroupCodeAsync(string groupCode);
+
         GroupApiResult Get(GroupModel entity);
         GroupApiResult GetByGroupCode(string groupCode);
+
         Task<GroupApiResult> GetAsync(GroupModel entity);
         Task<GroupApiResult> GetByGroupCodeAsync(string groupCode);
+
         GroupApiResult Read(GroupModel entity);
-        GroupApiResult ReadByGroupCode(string groupCode);
         Task<GroupApiResult> ReadAsync(GroupModel entity);
+
+        GroupApiResult ReadByGroupCode(string groupCode);
         Task<GroupApiResult> ReadByGroupCodeAsync(string groupCode);
+
         GroupApiResult Update(GroupModel entity);
-        GroupApiResult Update(string groupCode = null, string groupName = null, string subaccount = null, string department = null);
+        GroupApiResult Update(
+            string groupCode = null, 
+            string groupName = null,
+            string subaccount = null, 
+            string department = null
+        );
+
         Task<GroupApiResult> UpdateAsync(GroupModel entity);
-        Task<GroupApiResult> UpdateAsync(string groupCode = null, string groupName = null, string subaccount = null, string department = null);
+        Task<GroupApiResult> UpdateAsync(
+            string groupCode = null, 
+            string groupName = null, 
+            string subaccount = null, 
+            string department = null
+        );
 
         GroupListApiResult List();
         GroupListApiResult List(IListRequestOptions options);

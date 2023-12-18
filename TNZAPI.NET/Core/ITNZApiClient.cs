56e782905bcd41c4b6706c0e0f4086b63ecf801f
Paul KH KIM <babo_81@hotmail.com>
2023-12-19 12:31:08 +1300

    Added ITNZApiClient

---
+++ TNZAPI.NET/Core/ITNZApiClient.cs
@@ -0,0 +1,12 @@
+﻿using TNZAPI.NET.Core.Interfaces;
+
+namespace TNZAPI.NET.Core
+{
+	public interface ITNZApiClient
+	{
+		IActionsApi Actions { get; set; }
+		IAddressbookApi Addressbook { get; set; }
+		IMessagingApi Messaging { get; set; }
+		IReportsApi Reports { get; set; }
+	}
+}
--- TNZAPI.NET/Core/TNZApiClient.cs
+++ TNZAPI.NET/Core/TNZApiClient.cs
@@ -6,73 +6,73 @@ using TNZAPI.NET.Core.Interfaces;
 
 namespace TNZAPI.NET.Core
 {
-    public class TNZApiClient
-    {
-        private ITNZAuth User { get; set; }
+	public class TNZApiClient : ITNZApiClient
+	{
+		private ITNZAuth User { get; set; }
 
-        public IAddressbookApi Addressbook { get; set; }
-        public IActionsApi Actions { get; set; }
-        public IReportsApi Reports { get; set; }
-        public IMessagingApi Messaging { get; set; }
+		public IAddressbookApi Addressbook { get; set; }
+		public IActionsApi Actions { get; set; }
+		public IReportsApi Reports { get; set; }
+		public IMessagingApi Messaging { get; set; }
 
-        /// <summary>
-        /// Initiates Email message to send through TNZAPI
-        /// </summary>
-        public TNZApiClient()
-        {
-        }
+		/// <summary>
+		/// Initiates Email message to send through TNZAPI
+		/// </summary>
+		public TNZApiClient()
+		{
+		}
 
-        /// <summary>
-        /// Initiates Email message to send through TNZAPI
-        /// </summary>
-        /// <param name="authToken">Auth Token for TNZAPI</param>
-        public TNZApiClient(string authToken)
-        {
-            User.AuthToken = authToken;
+		/// <summary>
+		/// Initiates Email message to send through TNZAPI
+		/// </summary>
+		/// <param name="authToken">Auth Token for TNZAPI</param>
+		public TNZApiClient(string authToken)
+		{
+			User.AuthToken = authToken;
 
-            Initialise();
-        }
+			Initialise();
+		}
 
-        /// <summary>
-        /// Initiates Email message to send through TNZAPI
-        /// </summary>
-        /// <param name="sender">API Username - Email Address</param>
-        /// <param name="apiKey">API Key for TNZAPI</param>
-        public TNZApiClient(string sender, string apiKey)
-        {
-            User.Sender = sender;
-            User.APIKey = apiKey;
+		/// <summary>
+		/// Initiates Email message to send through TNZAPI
+		/// </summary>
+		/// <param name="sender">API Username - Email Address</param>
+		/// <param name="apiKey">API Key for TNZAPI</param>
+		public TNZApiClient(string sender, string apiKey)
+		{
+			User.Sender = sender;
+			User.APIKey = apiKey;
 
-            Initialise();
-        }
+			Initialise();
+		}
 
-        /// <summary>
-        /// Initiates Email message to send through TNZAPI
-        /// </summary>
-        /// <param name="apiUser">API User Details</param>
-        public TNZApiClient(TNZApiUser apiUser)
-        {
-            User = apiUser;
+		/// <summary>
+		/// Initiates Email message to send through TNZAPI
+		/// </summary>
+		/// <param name="apiUser">API User Details</param>
+		public TNZApiClient(TNZApiUser apiUser)
+		{
+			User = apiUser;
 
-            Initialise();
-        }
+			Initialise();
+		}
 
-        /// <summary>
-        /// Initiates Email message to send through TNZAPI
-        /// </summary>
-        public TNZApiClient(ITNZAuth auth)
-        {
-            User = auth;
+		/// <summary>
+		/// Initiates Email message to send through TNZAPI
+		/// </summary>
+		public TNZApiClient(ITNZAuth auth)
+		{
+			User = auth;
 
-            Initialise();
-        }
+			Initialise();
+		}
 
-        private void Initialise()
-        {
-            Addressbook = new AddressbookApi(User);
-            Actions = new ActionsApi(User);
-            Reports = new ReportsApi(User);
-            Messaging = new MessagingApi(User);
-        }
-    }
+		private void Initialise()
+		{
+			Addressbook = new AddressbookApi(User);
+			Actions = new ActionsApi(User);
+			Reports = new ReportsApi(User);
+			Messaging = new MessagingApi(User);
+		}
+	}
 }
